ca3926645de986869f80875620f367994d1c22af
hrrichartru <hrrichartru@CCS31008-167607.local>
2022-02-01 13:12:17 -0700

    added call to main

--- shell_lab.py
+++ shell_lab.py
@@ -2,8 +2,11 @@ import sys
 import os
 import re 
 
+def main():
+    while(True):
+        commandToExecute = input("\n$")
+        if commandToExecute == "exit":
+            break
 
-while(True):
-    commandToExecute = input("\n$")
-    if commandToExecute == "exit":
-        break
+if '__main__' == __name__:
+    main()
