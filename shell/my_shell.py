140816747943ccb14e04d6b704b142172bfb8b82
hrrichartru <hrrichartru@CCS31008-167607.local>
2022-02-14 19:28:55 -0700

    Added PS1, made file executable, started working with redirection

--- shell/my_shell.py
+++ shell/my_shell.py
@@ -1,10 +1,17 @@
+#!/usr/bin/env python3 
+
+from base64 import decode
 import sys 
 import os
-import re
 
 def main():
-  
     while True:
+        if "PS1" in os.environ:
+            os.write(1, str.encode(os.environ["PS1"]))
+        else:
+            os.environ["PS1"] = "$"
+            os.write(1, str.encode(os.environ["PS1"]))
+        # pid = os.getpid()
         #splitting user input, making it an array
         path = f"{os.getcwd()} $ "
         os.write(1, path.encode())
@@ -14,34 +21,26 @@ def main():
             sys.exit(0)
             
         elif commandToExecute[0].decode() == "cd": 
-            cd_command(commandToExecute)        
-    # while True:
-        # rc = os.fork()
-        
-        # if rc < 0:
-        #     sys.exit(1)
-        
-        # elif rc == 0:
-        #     # os.write(1, ("$").encode())
-        #     # commandToExecute = os.read(1,100).strip().split()[0]
-
-        #     # else:
-        #     #     print(commandToExecute[0].decode()+ ": Command not found")
-
-        #     args = commandToExecute
-
-        #     for dir in re.split(":", os.environ['PATH']):  # try each directory in the path
-        #         program = "%s/%s" % (dir, args[0])
-        #         try:
-        #             os.execve(program, args, os.environ)  # try to exec program
-
-        #         except FileNotFoundError:  # ...expected
-        #             pass  # ...fail quietly
-        #     #os.write(2, ("Child:    Could not exec %s\n" % args[0]).encode())
-        #     sys.exit(1)
-        # else:  # parent (forked ok)
-        #     childPidCode = os.wait()
-
+            cd_command(commandToExecute) 
+                       
+        else:
+            rc = os.fork()
+            
+            
+                          
+# def execute_command(command):
+#     pid = os.getpid()
+#     rc = os.fork()
+#     if rc < 0:
+#         os.write(2, ("fork failed, returning %d\n" % rc).encode())
+#         sys.exit(1)    
+            
+#     elif rc == 0:
+#         os.write(1, ("Child: My pid==%d.  Parent's pid=%d\n" % 
+#             (os.getpid(), pid)).encode())
+#         #check if input contains < or >
+#         #code redirection of input or output
+       
 def cd_command(path):
     #if the len is > 1, will change to directory in index 1 of the command array 
     if len(path) > 1:
