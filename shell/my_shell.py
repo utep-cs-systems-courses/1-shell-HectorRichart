1fcdce4073d3aa90ab07cad3ae8db22d1828579d
hrrichartru <hrrichartru@CCS31008-167607.local>
2022-02-14 20:46:16 -0700

    redirect almost working, couple bugs

--- shell/my_shell.py
+++ shell/my_shell.py
@@ -1,56 +1,66 @@
 #!/usr/bin/env python3 
-
 from base64 import decode
 import sys 
 import os
+import re 
 
 def main():
     while True:
-        if "PS1" in os.environ:
-            os.write(1, str.encode(os.environ["PS1"]))
-        else:
-            os.environ["PS1"] = "$"
-            os.write(1, str.encode(os.environ["PS1"]))
-        # pid = os.getpid()
-        #splitting user input, making it an array
+        #splitting user input, making it an array, printing prompt
         path = f"{os.getcwd()} $ "
         os.write(1, path.encode())
-        commandToExecute = os.read(0,1000).split()
+        #read user input, can read up to 1000 bytes, need to decode cause we get bytes back
+        commandToExecute = os.read(0,1000).decode().split()
         
-        if commandToExecute[0].decode() == "exit":
-            sys.exit(0)
+        if commandToExecute[0] == "exit":
+            sys.exit(1)
             
-        elif commandToExecute[0].decode() == "cd": 
-            cd_command(commandToExecute) 
-                       
-        else:
-            rc = os.fork()
+        elif commandToExecute[0] == "cd": 
+            cd_command(commandToExecute)
+            continue                      
             
+        #Fork a child, check if correctly forked    
+        rc = os.fork()
             
-                          
-# def execute_command(command):
-#     pid = os.getpid()
-#     rc = os.fork()
-#     if rc < 0:
-#         os.write(2, ("fork failed, returning %d\n" % rc).encode())
-#         sys.exit(1)    
+        if rc < 0:
+            sys.exit(1)
+                
+        #Executing in child process because rc == 0 by fork definition
+        elif rc == 0:
+            args = commandToExecute
+            #checking if the user input contains > which means redirect
+            if len(args) == 3 and args[1] == ">":
+                #redirect child stdout
+                os.close(1)             
+                #if specified file given by args[2] doers not exist create it or open to write only   
+                os.open(args[2], os.O_CREAT | os.O_WRONLY)
+                
+                os.set_inheritable(1, True)
+            for dir in re.split(":", os.environ['PATH']):  # try each directory in the path
+                program = "%s/%s" % (dir, args[0])
+                #using execve command to run the program on the os environment
+                try:
+                    os.execve(program, args, os.environ)  # try to exec program
+
+                except FileNotFoundError:
+                        pass           
+            sys.exit(1)      
+    #wait for child to finish
+        else: 
+            childPidCode = os.wait()  
             
-#     elif rc == 0:
-#         os.write(1, ("Child: My pid==%d.  Parent's pid=%d\n" % 
-#             (os.getpid(), pid)).encode())
-#         #check if input contains < or >
-#         #code redirection of input or output
-       
 def cd_command(path):
     #if the len is > 1, will change to directory in index 1 of the command array 
     if len(path) > 1:
         try:
             os.chdir(path[1])
+            #if specified directory does not exist
         except Exception:
-            print("cd: no such file or directory: {}".format(path[1].decode()))
+            print("cd: no such file or directory: {}".format(path[1]))
      #Takes us to the very main directory (cd, will go here if input is cd and just 1 thing in list)
     else:
        os.chdir(os.path.expanduser("~"))   
+       
 
 if '__main__' == __name__:
     main()
